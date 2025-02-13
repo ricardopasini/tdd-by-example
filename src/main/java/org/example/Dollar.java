888bcc2f6a7b0827fac91e6ab9b20d179a207e2f
Ricardo Pasini <ricardo.pasini@gmail.com>
2025-02-13 20:07:29 -0300

    First commit

---
+++ src/main/java/org/example/Dollar.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object obj) {
+        Dollar dollar = (Dollar) obj;
+        return amount == dollar.amount;
+    }
+}
---
+++ src/main/java/org/example/Franc.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Franc {
+
+    int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier){
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object obj) {
+        Franc franc = (Franc) obj;
+        return amount == franc.amount;
+    }
+}
