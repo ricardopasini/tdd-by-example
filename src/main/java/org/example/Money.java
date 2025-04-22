da6b2fb51a174ab62332f0ac6b7f1cde38bff49d
Ricardo Pasini <ricardo.pasini@gmail.com>
2025-04-21 21:10:09 -0300

    Compare Francs with Dollars

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -6,6 +6,7 @@ public class Money {
 
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount;
+        return amount == money.amount
+                && this.getClass().equals(obj.getClass());
     }
 }
